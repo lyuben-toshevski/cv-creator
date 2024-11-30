12d269b33a9cad3e44d76b8b312acf81661ce426
LyubenToshevski <lyuben.toshevski@gmail.com>
2024-11-30 09:54:06 +0200

    Add an injection token for the isMobile observable

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { IS_MOBILE_TOKEN, isMobileFactory } from '@shared/injection-tokens';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    {
+      provide: IS_MOBILE_TOKEN,
+      useFactory: isMobileFactory,
+      deps: [BreakpointObserver],
+    },
+  ],
 };
