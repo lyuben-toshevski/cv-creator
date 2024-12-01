e52e335bf697a579c903e93ec87aed767a3ee5fd
LyubenToshevski <lyuben.toshevski@gmail.com>
2024-12-01 21:38:06 +0200

    feature/sidebar Responsiveness updates, Render the new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
+import { IS_MOBILE_TOKEN } from '@shared/injection-tokens';
+import { Observable } from 'rxjs';
 import { CvContainerComponent } from 'src/cv-container/cv-container.component';
+import { HeaderComponent } from 'src/header/header.component';
+import { SidebarComponent } from 'src/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CvContainerComponent],
+  imports: [
+    CvContainerComponent,
+    HeaderComponent,
+    SidebarComponent,
+    NgIf,
+    AsyncPipe,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(@Inject(IS_MOBILE_TOKEN) public isMobile$: Observable<boolean>) {}
+}
